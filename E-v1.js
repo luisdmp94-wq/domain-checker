021fdccd97f8940d27d8631d493e2e37af97eea5
luisdmp94-wq <luisdmp94@gmail.com>
2026-07-09 12:53:20 +0200

    reemplazar Shodan por HackerTarget API gratuita

---
+++ E-v1.js
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Unhappy bird (404)</title>
+  <style>
+
+  html {
+    font-size: 100%;
+  }
+
+  body {
+    background: #f7f9fc;
+    font-size: 1em;
+    line-height: 1.4;
+  }
+
+  h1,
+  p {
+    margin: 0;
+  }
+
+  section.container {
+    position: relative;
+    top: 100px;
+    margin: 0 auto;
+    max-width: 820px;
+    text-align: center;
+  }
+
+  h1 {
+    position: relative;
+    color: #233241;
+    font-size: 2.8em;
+    font-family: 'Myriad Pro', sans-serif;
+    margin-bottom: 0.7em;
+  }
+
+  @media screen and (max-width: 768px) {
+    h1 {
+      font-size: 2.1em;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    h1 {
+      font-size: 1.4em;
+    }
+  }
+
+  h1 span {
+    color: #3291f0;
+  }
+
+  h1 span.number {
+    color: #91a5b9;
+  }
+
+  p {
+    position: relative;
+    font-size: 1.4em;
+    color: #91a5b9;
+    font-family: "Lucida Grande","Lucida Sans Unicode", "Helvetica Neue", Helvetica, sans-serif;
+  }
+
+  @media screen and (max-width: 480px) {
+    p {
+      font-size: 1em;
+    }
+  }
+
+  .sad-bird {
+    display: inline-block;
+    position: relative;
+    width: 25%;
+    padding-bottom: 25%;
+    vertical-align: middle;
+    overflow: hidden;
+    margin: 0 auto;
+  }
+
+  @media screen and (max-width: 480px) {
+   .sad-bird {
+      width: 35%;
+      padding-bottom: 35%;
+    }
+  }
+
+  @media screen and (max-width: 360px) {
+   .sad-bird {
+      width: 45%;
+      padding-bottom: 45%;
+    }
+  }
+
+  svg {
+    display: inline-block;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  a.back {
+    position: relative;
+    top: 4.2em;
+    font-size: 1.4em;
+    display: block;
+    color: #3291f0;
+    text-decoration: none;
+    font-family: "Lucida Grande","Lucida Sans Unicode", "Helvetica Neue", Helvetica, sans-serif;
+  }
+
+  @media screen and (max-width: 480px) {
+    a.back {
+      font-size: 1em;
+    }
+  }
+
+  a.back:hover {
+    color: #233241;
+    text-decoration: underline;
+  }
+
+  </style>
+</head>
+<body>
+  <section class="container">
+    <div class="sad-bird">
+      <svg viewBox="0 0 253 229" preserveAspectRatio="xMinYMin meet" xmlns="http://www.w3.org/2000/svg"><title>sad_bird</title><g fill="#3291F0" fill-rule="evenodd"><path d="M124 202c-50-4-66.3-31.2-70.8-50 0 0 9.7 2.6 22.6 4.3 0 0 3.4.5 3.5-.4.2-2-29-10.4-43-34.3-11-18.5-11.6-36.4-11.6-36.4C54.2 91 72.2 98 164.7 95.7c28.7 0 41.6 6 41.6 40.8 0 24.3-10.8 65-82.3 65.3zm128.3-67c0-4.4-5-7.4-11-9.2l.3-.2h-1l-2.8-1c-1.4-.3-2.8-1-4-2.2-1.2-1.5-2-3-2.4-4.6-4-25.4-18.3-48.4-66.6-48.4H81c-2.8 0-10.5-.5-28.7-2.5-17.6-2-33.5-7.4-33.6-7.5L3 53.8 1 70.3c-.2 2-5 48.2 24.7 71.5v.4c-1 12.3 3.5 39.2 29.5 61 56 47.5 132.4 17.7 152-3 12.2-13 21-28.6 24-47 .3-1.6 1.2-3.5 2.5-5l2-1.5c2-.5 4-1 6-1.8 5.8-2.4 10.7-5.4 10.7-10z"/><path d="M176.4 118.3l-6-6c-2-2-5.5-2-7.6 0-2 2-2 5.4 0 7.7l6 6-6 5.8c-2.3 2-2.3 5.5 0 7.7 2 2 5.4 2 7.6 0l6-6 5.8 6c2.2 2 5.5 2 7.7 0 2-2.2 2-5.5 0-7.8l-6-5.8 5.8-6c2.3-2 2.3-5.4 0-7.6-2-2-5.4-2-7.5 0l-6 6zM230 74.8c-2 0-3.5-1.7-3.2-4 0 0 0-.4.8-1.6.8-1.3 2.5-2.7 4.8-4.4 1.2-.8 2.3-1.7 3-2.8.7-1 1-2.4 1-3.8 0-1.6-.5-2.8-1.3-3.7-.7-1-2-1.4-3.5-1.4-1 0-2.3.4-3 1.2-.8.7-2 2-2.3 3-.5 1.4-5 2-7.7.3-3-1.8 1.2 2.3-.2-.3-1.4-2.6 1.2-6.6 3.6-8.4 2.3-2 5.5-2.8 9.5-2.8 4.3 0 7.7 1 10.2 3.2 2.4 2 3.7 5 3.7 8.7 0 2.2-.8 4.3-2 6-1.4 2-3 3.5-5.2 4.7-1 .8-1.8 1.5-2.3 2.3-.5 1 0 0 0 0-.4 2-2.4 3.6-4.5 3.6H230v.2zm5.3 7.7c0 2-1.5 3.6-3.6 3.6H230c-2.2 0-3.8-1.5-3.8-3.6 0-2 1.6-3.6 3.7-3.6h1.7c2 0 3.6 1.5 3.6 3.6zm-12-60.6c-2 0-3.5-1.8-3-4l.2-.4c.8-1 2-2.2 3.7-3.4 1-.6 1.7-1.3 2.3-2 .5-1 .8-2 .8-3.2 0-1-.4-2-1-2.7-.6-.8-1.5-1-2.6-1-.8 0-1.5.3-2.3.8-.5.7-1.4 1.6-1.8 2.5-.4 1-3.8 1.6-6 .2-2-1.3 1 1.8 0-.2s1-5 2.8-6.4c1.8-1.4 4-2 7.2-2 3.3 0 6 .8 7.7 2.5 2 1.6 3 3.8 3 6.6 0 1.8-.6 3.4-1.6 4.8-1 1.5-2.3 2.6-4 3.4-.8.6-1.4 1.2-1.7 2-.3.5 0 0 0 0-.3 1.5-2.3 2.6-4.3 2.6h.8zm3.2 5.7c0 1.6-1.3 3-2.8 3h-1.3c-1.5 0-2.8-1.3-2.8-3 0-1.4 1.3-2.6 2.8-2.6h1.4c1.5 0 3 1.4 3 3l-.3-.3zm-42.8 21c-2 0-3.6-1.8-3-4 0 0 0-1.4 1-3 1-1.7 3-3.4 6-5.5 1.4-.8 2.6-2 3.5-3.4.8-1.4 1.3-3 1.3-4.8 0-2-.5-3.3-1.5-4.6-1-1-2.5-1.5-4.3-1.5-1.6 0-2.7.3-4 1.2-.8 1-2.2 2.5-2.7 4s-6.2 2.3-9.6.2c-3.3-2 1.7 3 0-.2-1.8-3.2 1.3-8 4.3-10.4 3-2.4 7-3.6 12-3.6 5.4 0 9.6 1.3 12.6 4s4.6 6.3 4.6 11c0 2.7-1 5.3-2.6 7.6-1.8 2.3-3.8 4.2-6.4 5.5-1.6 1-2.3 2-3 3-.4 1 0 .7 0 .7-.4 2-2.4 3.8-4.6 3.8h-3.7v-.2zm7.5 9.3c0 2.6-2 4.6-4.6 4.6h-2.2c-2.4 0-4.6-2-4.6-4.6 0-2.4 2.2-4.6 4.6-4.6h2.3c2.5 0 4.5 2.2 4.5 4.6z"/></g></svg>
+    </div>
+
+    <h1><span>Moneybird</span> got lost <span class="number"> (404) </span></h1>
+    <p>
+      The page you were looking for could not be found.
+    </p>
+    <a href="javascript:history.back()" class="back">Go back to the previous page</a>
+
+  </section>
+</body>
+</html>
--- domain-checker.py
+++ domain-checker.py
@@ -532,6 +532,38 @@ def scan_js_files(domain):
         print(f"  Error: {e}")
     
     return findings
+
+def check_shodan(ip, api_key=None):
+    print(f"\nConsultando HackerTarget para {ip}:")
+    try:
+        r = requests.get(f"https://api.hackertarget.com/geoip/?q={ip}", timeout=5)
+        geo = {}
+        for line in r.text.split("\n"):
+            if ":" in line:
+                key, val = line.split(":", 1)
+                geo[key.strip()] = val.strip()
+
+        r2 = requests.get(f"https://api.hackertarget.com/hostsearch/?q={ip}", timeout=5)
+        hostnames = [h for h in r2.text.split("\n") if h.strip()]
+
+        results = {
+            "pais": geo.get("Country", "N/A"),
+            "ciudad": geo.get("City", "N/A"),
+            "org": geo.get("ASN", "N/A"),
+            "hostnames": hostnames[:5]
+        }
+
+        print(f"  Pais: {results['pais']}")
+        print(f"  Ciudad: {results['ciudad']}")
+        print(f"  ASN/Org: {results['org']}")
+        if results["hostnames"]:
+            for h in results["hostnames"]:
+                print(f"  Hostname: {h}")
+
+        return results
+    except Exception as e:
+        print(f"  Error: {e}")
+        return {}
 def generate_markdown(report):
     domain = report['dominio']
     fecha = report['fecha']
@@ -697,7 +729,8 @@ def main():
     http_methods = check_http_methods(domain)
     source_code = check_source_code(domain)
     js_findings = scan_js_files(domain)
-    risk_score = calculate_risk_score({"cabeceras_seguridad": security, "ssl": ssl_info, "waf": waf, "http_redirect": http_redirect, "archivos_sensibles": sensitive_files, "cors": cors, "http_methods": http_methods, "dns": dns_info, "codigo_fuente": source_code, "js_files": js_findings})
+    shodan_info = check_shodan(ip)
+    risk_score = calculate_risk_score({"cabeceras_seguridad": security, "ssl": ssl_info, "waf": waf, "http_redirect": http_redirect, "archivos_sensibles": sensitive_files, "cors": cors, "http_methods": http_methods, "dns": dns_info, "codigo_fuente": source_code, "js_files": js_findings, "shodan": shodan_info})
     
     report = {
         "dominio": domain,
@@ -716,7 +749,7 @@ def main():
         "cookies": cookies,
         "cors": cors,
         "http_methods": http_methods,
-        "codigo_fuente": source_code, "js_files": js_findings
+        "codigo_fuente": source_code, "js_files": js_findings, "shodan": shodan_info
     }
     
     save_report(report, domain)
@@ -724,6 +757,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
 
 
 
